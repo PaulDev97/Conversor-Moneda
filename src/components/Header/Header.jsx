a4c25e5590cf2e851a3a9eb0c0d372fa2b1a7731
PaulDev97 <paul.devc@gmail.com>
2023-08-10 21:55:44 -0300

    dolar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BoxPrice, SpanBlue } from './HeaderStyled'
 
 const Header = () => {
+
+  const [ dolarBlue , setDolarBlue] = useState([]);
+  const [ loading, setLoading ] = useState(true)
+
+
+  const GetDolar = async() => {
+    const url = 'https://api.bluelytics.com.ar/v2/latest';
+
+    const response = await fetch(url)
+
+    const data = await response.json()
+
+    
+    setDolarBlue([data])
+    setLoading(false)
+
+    
+    
+  }
+
+  console.log(dolarBlue)
+
+  useEffect(() => {
+    GetDolar()
+  },[])
+
   return (
     <div>
       <h1>Conversor Dolar <SpanBlue>Blue</SpanBlue></h1>
 
       <BoxPrice>
         <span>Dolar estadounidense: $0.0042</span>
-        <span>Dolar Blue: $488</span>
+
+        {
+          loading && <span>Cargando</span>
+        }
+        {
+          dolarBlue?.map((item) => (
+             <span>Dolar blue: ${item.blue.value_buy}</span>
+          ))
+          
+        }
+        
       </BoxPrice>
     </div>
   )
